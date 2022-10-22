42309d203962c76a26eba31a772bf60bae5eee8f
Duberly Guarnizo <duberlygfr@gmail.com>
2022-10-22 16:49:16 -0500

    Initial commit

---
+++ src/main/java/com/duberlyguarnizo/dgtablegenerator/DgtablegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.dgtablegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgtablegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgtablegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duberlyguarnizo/dgtablegenerator/DgtablegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.dgtablegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgtablegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
